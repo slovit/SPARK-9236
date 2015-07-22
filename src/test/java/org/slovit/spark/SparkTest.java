c719cecedc4fcabde6beb357a17ba88b012f2737
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2015-07-22 09:39:25 -0400

    Initial commit

---
+++ src/test/java/org/slovit/spark/SparkTest.java
@@ -0,0 +1,31 @@
+package org.slovit.spark;
+
+import java.util.Collections;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.Assert;
+import org.junit.Test;
+
+import scala.Tuple2;
+
+public class SparkTest {
+	
+	@Test
+	public void joinEmptyRDDTest() {
+		SparkConf sparkConf = new SparkConf().setAppName("test").setMaster("local");
+		
+		try (JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+			JavaRDD<String> rdd1 = sparkContext.parallelize(Collections.singletonList("one"));
+			JavaRDD<String> rdd2 = sparkContext.emptyRDD();
+			
+			JavaPairRDD<Integer, String> pair1 = rdd1.mapToPair(t -> new Tuple2<Integer, String>(1, t));
+			JavaPairRDD<Integer, Iterable<String>> pair2 = rdd2.groupBy(t -> 2);
+			
+			Assert.assertFalse(pair1.leftOuterJoin(pair2).collect().isEmpty());
+		}
+	}
+
+}
