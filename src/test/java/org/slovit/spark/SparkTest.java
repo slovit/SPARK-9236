de7bc4acecfbc1715712bb8fb07190777a4a3700
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2015-07-22 09:51:48 -0400

    Added successful test

--- src/test/java/org/slovit/spark/SparkTest.java
+++ src/test/java/org/slovit/spark/SparkTest.java
@@ -13,6 +13,9 @@ import scala.Tuple2;
 
 public class SparkTest {
 	
+	/**
+	 * Failing
+	 */
 	@Test
 	public void joinEmptyRDDTest() {
 		SparkConf sparkConf = new SparkConf().setAppName("test").setMaster("local");
@@ -20,6 +23,25 @@ public class SparkTest {
 		try (JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
 			JavaRDD<String> rdd1 = sparkContext.parallelize(Collections.singletonList("one"));
 			JavaRDD<String> rdd2 = sparkContext.emptyRDD();
+			
+			JavaPairRDD<Integer, String> pair1 = rdd1.mapToPair(t -> new Tuple2<Integer, String>(1, t));
+			JavaPairRDD<Integer, Iterable<String>> pair2 = rdd2.groupBy(t -> 2);
+			
+			Assert.assertFalse(pair1.leftOuterJoin(pair2).collect().isEmpty());
+		}
+	}
+	
+	/**
+	 * Expected behavior
+	 */
+	@Test
+	public void joinEmptyRDDTest_success() {
+		SparkConf sparkConf = new SparkConf().setAppName("test").setMaster("local");
+		
+		try (JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+			JavaRDD<String> rdd1 = sparkContext.parallelize(Collections.singletonList("one"));
+			// The only difference between the tests
+			JavaRDD<String> rdd2 = sparkContext.parallelize(Collections.singletonList("two"));
 			
 			JavaPairRDD<Integer, String> pair1 = rdd1.mapToPair(t -> new Tuple2<Integer, String>(1, t));
 			JavaPairRDD<Integer, Iterable<String>> pair2 = rdd2.groupBy(t -> 2);
